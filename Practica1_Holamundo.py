ac1cd5d3f41225ed6a9751f9ed8ce514bc006d0a
Adan Mercado <adan_mercado11@outlook.com>
2022-07-10 21:16:04 -0500

    primer commit de la python

---
+++ Practica1_Holamundo.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 00:43:15 2020
+
+@author: Adan
+"""
+
+#print ("Hola Mundo")
+
+# ***numeros inmutables***
+# int = entero
+# long = entero long
+# float = coma flotante
+# complex = complejo
+# bool = booleano
+
+# ***secuencias inmutables***
+# str = cadena de caracteres 
+# unicode = cadena de caracteres unicode
+
+
+# print ("***Realizando Operaciones***")
+
+# num_1 = int(input("Inserta un numero: ")) 
+# #introducimos un numero 
+
+# num_2 = int(input("Inseta otro numero: ")) 
+# #introducimos otro numero
+
+# resultado = num_1 + num_2 
+# #realizamos operacion
+
+# print ("El resultado de la suma es: ", resultado)
+# #imprimimos el resultado de la operacion
+
+nom_1 = str(input("Introduce tu nombre: "))
+
+num_2 = int(input("Introduce tu edad: "))
+
+print ("\nTu nombre es: ", nom_1)
+print ("\nTu edad es: ", num_2 ,"años")
+print ("Hola Mundo")
